b3c8e790b8c17352da18f48e301fdf1f488da279
urbanpixellab <enrico@urbanpixellab.com>
2021-12-01 19:21:47 +0100

    added stepper control, to hold all stpper commands

--- CatLaserDisco.ino
+++ CatLaserDisco.ino
@@ -6,7 +6,7 @@
 // Define number of steps per rotation:
 const int stepsPerRevolution = 2048;
 Stepper hStepper = Stepper(stepsPerRevolution, 5, 23, 18, 19);//heading
-Stepper pStepper = Stepper(stepsPerRevolution, 13,14,12,27);//pitch
+Stepper pStepper = Stepper(stepsPerRevolution, 13, 14, 12, 27); //pitch
 const int laserPin = 21;
 
 // Replace with your network credentials
@@ -40,13 +40,13 @@ void setup() {
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
-  
+
   server.begin();
   hStepper.setSpeed(10);
   pStepper.setSpeed(10);
 }
 
-void loop(){
+void loop() {
   WiFiClient client = server.available();   // Listen for incoming clients
 
   if (client) {                             // If a new client connects,
@@ -67,7 +67,7 @@ void loop(){
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
-            
+
             // turns the GPIOs on and off
             if (header.indexOf("GET /26/on") >= 0) {
               output26State = "on";
@@ -93,45 +93,45 @@ void loop(){
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
+            // CSS to style the on/off buttons
             // Feel free to change the background-color and font-size attributes to fit your preferences
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
             client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
             client.println(".button2 {background-color: #555555;}</style></head>");
-            
+
             // Web Page Heading
             client.println("<body><h1>ESP32 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 26  
+
+            // Display current state, and ON/OFF buttons for GPIO 26
             client.println("<p>GPIO 26 - State " + output26State + "</p>");
-            // If the output26State is off, it displays the ON button       
-            if (output26State=="off") {
+            // If the output26State is off, it displays the ON button
+            if (output26State == "off") {
               client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 27  
+            }
+
+            // Display current state, and ON/OFF buttons for GPIO 27
             client.println("<p>GPIO 27 - State " + output27State + "</p>");
-            // If the output27State is off, it displays the ON button       
-            if (output27State=="off") {
+            // If the output27State is off, it displays the ON button
+            if (output27State == "off") {
               client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
 
-            // Display current state, and ON/OFF buttons for Laser  
+            // Display current state, and ON/OFF buttons for Laser
             client.println("<p>GPIO 27 - State " + output28State + "</p>");
-            // If the output27State is off, it displays the ON button       
-            if (output28State=="off") {
+            // If the output27State is off, it displays the ON button
+            if (output28State == "off") {
               client.println("<p><a href=\"/28/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/28/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
 
             client.println("</body></html>");
-            
+
             // The HTTP response ends with another blank line
             client.println();
             // Break out of the while loop
